701e386dd7c7315ea08df31170c543e2f3f98da9
Jaybee <asd1213719@gmail.com>
2025-07-14 15:24:58 +0900

    feat : login

--- frontend/src/pages/public/Login.jsx
+++ frontend/src/pages/public/Login.jsx
@@ -4,6 +4,8 @@ import { login as loginAPI } from '@/services/authAPI';
 import { Link } from 'react-router-dom';
 import logo from '@/assets/ome.png';
 import { useAuth } from '@/hooks/useAuth'; // 실제 사용할 경우만 유지
+import ProgressButton from '@/components/ProgressButton';
+
 
 export default function Login() {
   const [id, setId] = useState('');
@@ -42,81 +44,72 @@ export default function Login() {
 
   };
 
-
-
-
   return (
-    <>
-    <ProgressBar start={isLoading} />
-    <div className="flex items-center justify-center min-h-screen bg-gray-50">
-      <div className="w-full max-w-sm p-8 bg-white rounded-lg shadow">
-        {/* 로고 */}
-        <div className="flex items-center justify-center mb-1">
-          <Link to="/">
-            <img
-              src={logo}
-              alt="OME 로고"
-              className="h-20 w-auto mr-2 cursor-pointer"
-            />
-          </Link>
+      <div className="flex items-center justify-center min-h-screen bg-gray-50">
+        <div className="w-full max-w-sm p-8 bg-white rounded-lg shadow">
+          {/* 로고 */}
+          <div className="flex items-center justify-center mb-1">
+            <Link to="/">
+              <img
+                src={logo}
+                alt="OME 로고"
+                className="h-20 w-auto mr-2 cursor-pointer"
+              />
+            </Link>
+          </div>
+          <form className="space-y-4" onSubmit={handleLogin}>
+            <div>
+              <label className="block mb-1 text-sm font-medium text-gray-700">ID</label>
+              <input
+                type="text"
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+                placeholder="아이디를 입력하세요."
+                className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-green-500"
+              />
+            </div>
+
+            <div>
+              <label className="block mb-1 text-sm font-medium text-gray-700">Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="비밀번호를 입력하세요."
+                className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-green-500"
+              />
+            </div>
+
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                id="remember"
+                className="w-4 h-4 mr-2 text-green-600"
+              />
+              <label htmlFor="remember" className="text-sm text-gray-700">
+                아이디 저장
+              </label>
+            </div>
+
+            <ProgressButton isLoading={isLoading} type="submit">
+              로그인
+            </ProgressButton>
+
+            <Link to="/signup">
+              <button
+                type="button"
+                className="w-full py-2 text-white bg-yellow-500 rounded hover:bg-yellow-600"
+              >
+                회원가입
+              </button>
+            </Link>
+            {errorMessage && (
+              <p className="text-red-500 text-sm text-center mt-2">
+                {errorMessage}
+              </p>
+            )}
+          </form>
         </div>
-        <form className="space-y-4" onSubmit={handleLogin}>
-          <div>
-            <label className="block mb-1 text-sm font-medium text-gray-700">ID</label>
-            <input
-              type="text"
-              value={id}
-              onChange={(e) => setId(e.target.value)}
-              placeholder="아이디를 입력하세요."
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-green-500"
-            />
-          </div>
-
-          <div>
-            <label className="block mb-1 text-sm font-medium text-gray-700">Password</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="비밀번호를 입력하세요."
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-green-500"
-            />
-          </div>
-
-          <div className="flex items-center">
-            <input
-              type="checkbox"
-              id="remember"
-              className="w-4 h-4 mr-2 text-green-600"
-            />
-            <label htmlFor="remember" className="text-sm text-gray-700">
-              아이디 저장
-            </label>
-          </div>
-
-          <button
-            type="submit"
-            className="w-full py-2 text-white bg-green-600 rounded hover:bg-green-700"
-          >
-            로그인
-          </button>
-
-          <Link to="/signup">
-            <button
-              type="button"
-              className="w-full py-2 text-white bg-yellow-500 rounded hover:bg-yellow-600"
-            >
-              회원가입
-            </button>
-          </Link>
-          {errorMessage && (
-            <p className="text-red-500 text-sm text-center mt-2">
-              {errorMessage}
-            </p>
-          )}
-        </form>
       </div>
-    </div>
-    </>
   );
 }
