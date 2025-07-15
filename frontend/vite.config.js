f598e82495834df78023d9ac989a6f9b860b9439
Jaybee <asd1213719@gmail.com>
2025-07-15 19:36:14 +0900

    update : header-logo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import path from 'path';
 import { fileURLToPath } from 'url';
+import svgr from 'vite-plugin-svgr';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,6 +12,7 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
+    svgr(),
   ],
   resolve: {
     alias: {
