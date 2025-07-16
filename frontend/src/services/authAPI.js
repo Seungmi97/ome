a4df0bccfc73c27e4c7daf2e584e17f062132c51
Jaybee <asd1213719@gmail.com>
2025-07-16 18:34:33 +0900

    login-signup update

--- frontend/src/services/authAPI.js
+++ frontend/src/services/authAPI.js
@@ -2,14 +2,14 @@ import api from './api';
 
 /**
  * 로그인 요청
- * @param {{ id: string, password: string }} payload
+ * @param {{ user_id: string, password: string }} payload
  * @returns {Promise<{ accessToken: string }>}
  */
 export const login = (payload) => api.post('/auth/login', payload);
 
 /**
  * 회원가입 요청
- * @param {{ id: string, password: string, email: string, role: string, username: string, applyAsCreator: boolean }} payload
+ * @param {{ user_id: string, password: string, email: string, role: string, username: string, applyAsCreator: boolean }} payload
  * @returns {Promise<any>}
  */
 export const signup = (payload) => api.post('/auth/signup', payload);
