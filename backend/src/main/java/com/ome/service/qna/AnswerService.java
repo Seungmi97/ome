da90055cd6be9e89e2de1a177d149ff0bc2c4936
Seungmi97 <mi.seung97@gmail.com>
2025-07-21 15:24:51 +0900

    [Feat] 답변 등록

---
+++ backend/src/main/java/com/ome/service/qna/AnswerService.java
@@ -0,0 +1,44 @@
+package com.ome.service.qna;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Service;
+
+import com.ome.domain.Answer;
+import com.ome.domain.Question;
+import com.ome.domain.Users;
+import com.ome.dto.qna.request.AnswerRequestDto;
+import com.ome.repository.auth.UserRepository;
+import com.ome.repository.qna.QuestionRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AnswerService {
+	
+	private final QuestionRepository questionRepository;
+	private final UserRepository userRepository;
+	
+	@Transactional
+	public String createAnswer(Long questionId, AnswerRequestDto requestDto, Long userId) {
+		
+		Question question = questionRepository.findById(questionId).orElseThrow(() -> new RuntimeException("존재하지 않는 질문글입니다"));
+		
+		if(question.getRecipe().getWriter().getId() != userId) {
+			throw new AccessDeniedException("권한이 없습니다");
+		}
+		
+		Users creator = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("존재하지 않는 사용자입니다"));
+		
+		Answer answer = new Answer();
+		answer.setCreator(creator);
+		answer.setContent(requestDto.getContent());
+		
+		question.setAnswer(answer);
+		questionRepository.save(question);
+		
+		return "답변이 등록되었습니다";
+	}
+ 
+}
