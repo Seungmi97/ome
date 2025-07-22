9f84cd16ff007b19455081bfe5b289678f598e56
Seungmi97 <mi.seung97@gmail.com>
2025-07-22 21:08:52 +0900

    [Feat] 신고 조회

--- backend/src/main/java/com/ome/controller/report/ReportController.java
+++ backend/src/main/java/com/ome/controller/report/ReportController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import com.ome.dto.report.request.ReportRequestDto;
+import com.ome.dto.report.response.ReportResponseDto;
 import com.ome.service.auth.CustomUserDetails;
 import com.ome.service.report.ReportService;
 
@@ -21,5 +22,11 @@ public class ReportController {
 	public ResponseEntity<String> createReport(@RequestBody ReportRequestDto requestDto,
 											   @AuthenticationPrincipal CustomUserDetails user){
 		return ResponseEntity.ok(reportService.createReport(requestDto, user.getUser()));
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ReportResponseDto> getReport(@PathVariable Long id,
+													   @AuthenticationPrincipal CustomUserDetails user){
+		return ResponseEntity.ok(reportService.getReport(id, user.getId()));
 	}
 }
--- backend/src/main/java/com/ome/service/report/ReportService.java
+++ backend/src/main/java/com/ome/service/report/ReportService.java
@@ -7,6 +7,10 @@ import com.ome.common.enums.ReportTargetType;
 import com.ome.domain.Report;
 import com.ome.domain.Users;
 import com.ome.dto.report.request.ReportRequestDto;
+import com.ome.dto.report.response.ReportResponseDto;
+import com.ome.repository.auth.UserRepository;
+import com.ome.repository.comment.CommentRepository;
+import com.ome.repository.recipe.RecipeRepository;
 import com.ome.repository.report.ReportRepository;
 
 import jakarta.transaction.Transactional;
@@ -17,6 +21,9 @@ import lombok.RequiredArgsConstructor;
 public class ReportService {
 
 	private final ReportRepository reportRepository;
+	private final RecipeRepository recipeRepository;
+	private final CommentRepository commentRepository;
+	private final UserRepository userRepository;
 	
 	@Transactional
 	public String createReport(ReportRequestDto requestDto, Users user) {
@@ -29,10 +36,13 @@ public class ReportService {
 		switch(requestDto.getTargetType()) {
 		case "recipe":
 			report.setTargetType(ReportTargetType.RECIPE);
+			break;
 		case "comment":
 			report.setTargetType(ReportTargetType.COMMENT);
+			break;
 		case "user":
 			report.setTargetType(ReportTargetType.USER);
+			break;
 		}
 		
 		report.setReason(requestDto.getReason());
@@ -41,5 +51,28 @@ public class ReportService {
 		reportRepository.save(report);
 		
 		return "신고가 접수되었습니다";
+	}
+
+	@Transactional
+	public ReportResponseDto getReport(Long id, Long userId) {
+		
+		Report report = reportRepository.findById(id).orElseThrow(() -> new RuntimeException("존재하지 않는 신고글입니다"));
+		String target;
+		
+		switch(report.getTargetType()) {
+		case ReportTargetType.RECIPE:
+			target = recipeRepository.findById(report.getTargetId()).orElseThrow(() -> new RuntimeException("존재하지 않는 레시피입니다")).getTitle();
+			break;
+		case ReportTargetType.COMMENT:
+			target = commentRepository.findById(report.getTargetId()).orElseThrow(() -> new RuntimeException("존재하지 않는 댓글입니다")).getContent();
+			break;
+		case ReportTargetType.USER:
+			target = userRepository.findById(report.getTargetId()).orElseThrow(() -> new RuntimeException("존재하지 않는 사용자입니다")).getUsername();
+			break;
+		default:
+			target = "";
+		}
+		
+		return ReportResponseDto.from(report, target);
 	}
 }
